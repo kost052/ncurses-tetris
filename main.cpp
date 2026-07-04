869d575aba257adc0e9d6921a36827c6d7745fc0
kost052 <kornel.staniszewski52@gmail.com>
2026-07-04 11:44:33 +0200

    updated

--- main.cpp
+++ main.cpp
@@ -1,153 +1,82 @@
 #include <vector>
-#include <random>
 #include <algorithm>
-#include <string>
+#include <random>
 #include <ncurses.h>
 #include "tetrominos.h"
 
 using namespace std;
+using tetromino = vector<vector<bool>>;
 using pos = unsigned int;
-
-constexpr int enter = 10;
 
 constexpr pos rows = 20, columns = 10;
 
-vector<vector<bool>> matrix(rows, vector<bool>(columns, 0)), matrix_tmp = matrix;
+vector<vector<bool>> matrix(rows, vector<bool>(columns, 0));
 
-void show_matrix(pos x = 0, pos y = 0) {
-    clear();
-    move(y, x);
-    for(const vector<bool> &row : matrix_tmp) {
-        for(const bool &state : row) {
-            if(state) {
-                addstr("[]");
-            } else {
-                addstr(" .");
-            }
-        }
-        y++;
-        move(y, x);
-    }
-    refresh();
-}
-
-class Tetromino {
+class tetro {
     public:
-        int x, y;
         tetromino shape;
-        bool falling = false;
+        
+        void insert(pos x, pos y) {
+            if(collides) return;
 
-        bool insert(int c, int r) {
-            int xov = c, yov = r;
-            matrix_tmp = matrix;
-            for(const vector<bool> &row : shape) {
-                int i = c;
-                for(const bool &state : row) {
-                    if(state) {
-                        if(i >= 0 && i < columns && r < rows && matrix_tmp[r][i] == 0) {
-                            matrix_tmp[r][i] = state;
-                        } else {
-                            matrix_tmp = matrix;
-                            insert(x, y);
-                            return false;
-                        }
-                    }
-                    i++;
+            clear(pos_x, pos_y);
+
+            loop(x, y, [this](pos x, pos y) {
+                if(shape[y][x]) {
+                    matrix[y][x] = 1;
                 }
-                r++;
-            }
-            x = xov;
-            y = yov;
-            return true;
-        }
+            });
 
-        void init() {
-            insert((columns-shape.size())/2+1, 0);
-        }
-
-        void move(char dir) {
-            switch(dir) {
-                case 'D':
-                    insert(x, y+1);
-                    break;
-                case 'L':
-                    insert(x-1, y);
-                    break;
-                case 'R':
-                    insert(x+1 , y);
-                    break;
-            }
-        }
-
-        void rotate() {
-            tetromino rotated = shape, tmp = shape;
-
-            for(vector<bool> &row : shape) {
-                pos i = 0;
-                for(bool state : row) {
-                    rotated[i][shape.size()-1-i] = 1;
-                }
-            }
-            
-            shape = rotated;
-        }
-};
-
-int main() {
-    initscr();
-    cbreak();
-    noecho();
-
-    bool running = true, playing = false;
-
-    random_device rng;
-    
-    vector<tetromino> shuffled;
-
-    while(running) {
-        show_matrix();
-
-        switch(getch()) {
-            case enter:
-                playing = true;
-                break;
-            case 'q':
-                running = false;
-                break;
+            pos_x = x;
+            pos_y = y;
         }
         
-        while(playing) {
-            if(shuffled.size() == 0) {
-                shuffled = tetros;
-                shuffle(shuffled.begin(), shuffled.end(), rng);
+        void rotate() {
+            tetromino origin = shape;
+
+            loop(0, 0, [&](pos x, pos y) {
+                shape[x][size-1-y] = origin[y][x];
+            });
+
+            vector<vector<bool>> matrix_origin = matrix;
+
+            clear(pos_x, pos_y);
+
+            if(collides(pos_x, pos_y)) {
+                shape = origin;
+                matrix = matrix_origin;
+                return;
+            } else {
+                insert(pos_x, pos_y);
             }
+        }
 
-            Tetromino tetro;
-            tetro.shape = shuffled[0];
+    pos pos_x, pos_y;
+    pos size = shape.size();
 
-            shuffled.erase(shuffled.begin());
-
-            tetro.falling = true;
-            tetro.init();
-
-            while(tetro.falling) {
-                show_matrix();
-                
-                switch(getch()) {
-                    case 's':
-                        tetro.move('D');
-                        break;
-                    case 'a':
-                        tetro.move('L');
-                        break;
-                    case 'd':
-                        tetro.move('R');
-                        break;
-                    case 'w':
-                        tetro.rotate();
-                        break;
-                }
+    template <typename function>
+    void loop(pos startx, pos starty, function func) {
+        for(pos row = starty; row < size; ++row) {
+            for(pos col = startx; col < size; ++col) {
+                func(row, col);
             }
         }
     }
-}
+
+    bool collides(pos x, pos y) {
+        loop(x, y, [this](pos x, pos y) {
+            if(shape[y][x]) {
+                if(matrix[y][x] == 1 || y < 0 || y >= rows || x < 0 || x >= columns) {
+                    return true;
+                }
+            }
+        });
+        return false;
+    }
+
+    void clear(pos x, pos y) {
+        loop(x, y, [](pos x, pos y) {
+            matrix[y][x] = 0;
+        });
+    }
+};
